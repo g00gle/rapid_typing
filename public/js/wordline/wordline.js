a162d7fe9a4803a5dc3daafb9680184af5343610
g00gle <70999506+g00gle@users.noreply.github.com>
2021-05-16 21:55:26 -0700

    Update wordline.js

--- public/js/wordline/wordline.js
+++ public/js/wordline/wordline.js
@@ -16,7 +16,12 @@ export default class Wordline {
     this.errorStats = new ErrorStats();
     this.speedStats = new SpeedStats();
     this.errorCounter = new Counter();
-    this.generator = new Generator({ interval: 6e4, number: 1 });
+    if (this.mode = "beginner") {
+       this.generator = new Generator({ interval: 6e4, number: 1 });  
+    } else {
+      this.generator = new Generator({ interval: 6e4, number: 8 });
+    }
+    
     this.keyboard = new Keyboard();
     this.letters = '';
 
